297e811df8a78c11a804d76414155503d81c5be8
Srikanth Kamuju <Srikanth.Kamuju@genzeon.com>
2021-12-24 15:24:31 +0530

    Add project files.

---
+++ AspMVCCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspMVCCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
